6254a9aef424cb66961eb1200e06e38d6ad7a91a
Shashank Wadhwa <shashankwadhwa9@gmail.com>
2021-08-19 06:08:05 +0530

    run glue crawlers for raw data cataloging

---
+++ src/3_run_glue_crawlers_raw_catalog.sh
@@ -0,0 +1,4 @@
+aws glue start-crawler --name smart-hub-locations-csv
+aws glue start-crawler --name smart-hub-sensor-mappings-json
+aws glue start-crawler --name smart-hub-data-json
+aws glue start-crawler --name smart-hub-rates-xml
